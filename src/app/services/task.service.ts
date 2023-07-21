6472c460a15368d06ff66a5a36772279f2cdefee
shahid <razashahid532@gmail.com>
2023-07-21 16:59:28 +0530

    Added HttpCLient, HttpHeader

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from "@angular/common/http"
 import { TASKS } from 'src/app/mock.task';
 import { Task } from 'src/app/Task';
 
@@ -7,9 +9,12 @@ import { Task } from 'src/app/Task';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-  getTasks(): Task[] {
-    return TASKS;
+  getTasks(): Observable<Task[]> {
+    // const task = of(TASKS);
+    // return task;
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
